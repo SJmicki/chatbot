6069e0818a973997252ccd34f9029689e7e5b242
SJmicki <89453893+SJmicki@users.noreply.github.com>
2025-02-02 09:17:10 +0000

    fixed bugs and improved workflow

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,6 @@ from bs4 import BeautifulSoup
 from openai import OpenAI
 from webscrape import WebScrape
 
-# def stream_data(corpus):
-#     for word in corpus.split(" "):
-#         yield word + " "
-#         time.sleep(0.02)
-
 # create request header
 headers = {'User-Agent': "Kevin.Peterson@bainbridge.com"}
 
@@ -23,7 +18,16 @@ filing_type = st.selectbox("Select filing type", ("10-K", "10-Q"),
                            index=None,
                             placeholder="i.e 10-K, 10-Q",)
 latest_nth_report = st.slider("Nth latest report.", 1, 20) - 1
-    
+
+if st.button("submit"):
+    filing = WebScrape(headers, ticker, filing_type, latest_nth_report)
+    st.session_state.mda = filing.extract_mda_section()
+    st.session_state.ticker = filing.ticker
+    st.session_state.report_name = filing.report_name
+    st.session_state.basic_info = f"""
+I will ask questions related {st.session_state['ticker']}. If I refer to financials without specification, it means the financial statements as of {st.session_state['report_name']}. Respond 'Got it' if you understand.
+"""
+
 st.write(
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
@@ -31,43 +35,44 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+
 prompt_prefix = """
-You are an financial investment expert with years of experience in the equity research space. You will be asked to evaluate the Management Discussion and Analysis section of public company financial statements. 
-Based on the report input and your financial expertise, provide 1) a summary of the MDA, 2) sentiment analysis of the report, 3) key highlights and low lights of the report. 
+You are an financial investment expert with years of experience in the equity research space. Only answer my questions when I explicitly ask so. When you respond, provide strctured responses that answers inquiries in a comprehensive manner that considers both profits and risks.
 """
 
+openai_api_key = st.text_input("OpenAI API Key", type="password")
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
-    
-    
-    if st.button("Submit"):
-        filing = WebScrape(headers, ticker, filing_type, latest_nth_report)
-        final_text = filing.extract_mda_section()
 
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
+    # Create a session state variable to store the chat messages. This ensures that the messages persist across reruns.
     if "messages" not in st.session_state:
         st.session_state.messages = []
+        st.session_state.messages.append({"role": "system", "content": prompt_prefix})
+        st.session_state.messages.append({"role": "user", "content": st.session_state.basic_info})
 
     # Display the existing chat messages via `st.chat_message`.
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
+            st.markdown(message["content"][:5000])
+    
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if final_text:
+    if prompt := st.chat_input("What is up?"):
         
-        prompt = prompt_prefix + final_text
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
-        # with st.chat_message("user"):
-        #     st.markdown(prompt)
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+    if st.button("MDA Analysis"):
+        mda_input = f"""You will be asked to evaluate the Management Discussion and Analysis section of public company financial statements. 
+Based on the report input and your financial expertise, provide 1) a summary of the MDA, 2) sentiment analysis of the report, 3) key highlights and low lights of the report. 
+This MDA analysis is related to {st.session_state['ticker']} - {st.session_state['report_name']}. Here is the report: {st.session_state['mda']}"""
+        st.session_state.messages.append({"role": "user", "content": mda_input})
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
@@ -75,12 +80,20 @@ else:
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
-            ],
+                ],
+            stream=True,
+        )
+    else:    
+        stream = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+                ],
             stream=True,
         )
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        # Stream the response to the chat using `st.write_stream`, then store it in session state.
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
